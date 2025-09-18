07c2ec072ddf623d5e07cae0c48c5089e07f81d6
KarthikSajwan <karthiksajwan03@gmail.com>
2025-09-18 17:41:01 +0530

    Added borrow functionality

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status, Path
 from typing import Annotated
 from sqlalchemy.orm import Session
 import models
-from models import Books, Members
+from models import Books, Members, BorrowRecords
 from database import engine, SessionLocal
 from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
@@ -33,6 +33,11 @@ class MemberRequest(BaseModel):
     name: str = Field(..., description="Member's full name")
     email: EmailStr = Field(..., description="Member's email address")
     joined_date: Optional[datetime] = Field(None, description="Date the member joined")
+
+class BorrowRequest(BaseModel):
+    member_id: int
+    book_id: int
+
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
@@ -116,3 +121,38 @@ async def delete_member(db: db_dependency, member_id: int = Path(gt=0)):
     db.query(Members).filter(Members.id == member_id).delete()
     db.commit()
 
+
+
+@app.post("/borrow", status_code=status.HTTP_201_CREATED)
+async def borrow_book(db: db_dependency, borrow_request: BorrowRequest):
+
+    book_model = db.query(Books).filter(Books.id == borrow_request.book_id).first()
+    if book_model is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    if book_model.available_copies <= 0:
+        raise HTTPException(status_code=400, detail="No copies available for this book")
+
+    # 3. Check if member exists
+    member_model = db.query(Members).filter(Members.id == borrow_request.member_id).first()
+    if member_model is None:
+        raise HTTPException(status_code=404, detail="Member not found")
+
+    borrow_model = BorrowRecords(
+        member_id=borrow_request.member_id,
+        book_id=borrow_request.book_id
+    )
+    db.add(borrow_model)
+
+    book_model.available_copies -= 1
+    if book_model.available_copies == 0:
+        book_model.is_available = False
+
+    db.commit()
+
+    return {
+        "message": "Book borrowed successfully",
+        "member": member_model.name,
+        "book": book_model.title,
+        "borrow_date": borrow_model.borrow_date
+    }
